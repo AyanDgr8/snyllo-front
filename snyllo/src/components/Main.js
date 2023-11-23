b657c2c8ee5157d5e0dd4ef38ba4401231759262
Ayan <ayandgr5@gmail.com>
2023-11-23 14:22:53 +0530

    header done

--- snyllo/src/components/Main.js
+++ snyllo/src/components/Main.js
@@ -1,15 +1,15 @@
 // src/components/Main.js
 
 import Header from './routes/Header';
-import First from './routes/First';
-import Second from './routes/Second'
+// import First from './routes/First';
+// import Second from './routes/Second'
 
 export default function Main(){
   return (
     <div>
         <Header />
-        <First />
-        <Second />
+        {/* <First />
+        <Second /> */}
     </div>
   );
 }
---
+++ snyllo/src/components/routes/First.js
@@ -0,0 +1,1 @@
+// src/components/routes/First.js
